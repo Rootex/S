e0a5c0eb1c84430c49d85e3ec110ea16d6f86045
Sotaya Yakubu <sotayamy@gmail.com>
2016-10-24 17:49:36 +0200

    More func

--- src/main.go
+++ src/main.go
@@ -15,3 +15,19 @@ func (p *Page) save() error {
 	fmt.Printf("Writing %s to file", p.Title)
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	page1 := &Page{Title: "Testpage", Body: []byte("Data sample page")}
+	page1.save()
+	load1, _ := loadPage("Testpage")
+	fmt.Println(load1.Body)
+}
